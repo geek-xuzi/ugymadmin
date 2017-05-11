ba03a678eaf001c60f09994471412fe8742605fa
xuzheng <xuzheng@benmu-health.com>
2017-05-11 13:43:13 +0800

    增加本地缓存

--- src/main/java/com/ugym/admin/service/DataServiceImpl.java
+++ src/main/java/com/ugym/admin/service/DataServiceImpl.java
@@ -1,8 +1,13 @@
 package com.ugym.admin.service;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.ugym.admin.bean.Motion;
 import com.ugym.admin.dao.MotionDao;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +20,31 @@ import java.util.Map;
 @Service
 public class DataServiceImpl implements DataService {
 
+    private static final long EXPIRE = 30000;
+    private static final String MOTION_KEY = "XUEN";
+
+
     @Autowired
     private MotionDao motionDao;
 
+    private LoadingCache<String, List<Motion>> motionCache = CacheBuilder.newBuilder()
+            .expireAfterAccess(EXPIRE, TimeUnit.MILLISECONDS).maximumSize(100)
+            .build(new CacheLoader<String, List<Motion>>() {
+                @Override
+                public List<Motion> load(String s) throws Exception {
+                    return motionDao.getMotionData();
+                }
+            });
+
     public List<Motion> getData() {
         long start = System.currentTimeMillis();
-        List<Motion> motionData = motionDao.getMotionData();
-        System.out.println(System.currentTimeMillis() - start);
-        return motionData;
+        try {
+            return motionCache.get(MOTION_KEY);
+        } catch (ExecutionException e) {
+            return motionDao.getMotionData();
+        } finally {
+            System.out.println(System.currentTimeMillis() - start);
+
+        }
     }
 }
